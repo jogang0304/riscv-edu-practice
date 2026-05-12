f37cb66654cd10cc6c8d3b01e512370cadbab95e
jogang <jogang0304@gmail.com>
2026-05-12 21:59:06 +0300

    done hw 1

--- 5_opensbi.sh
+++ 5_opensbi.sh
@@ -3,7 +3,7 @@
 . ./config.sh
 
 cd opensbi
-git clean -fdx
+# git clean -fdx
 make CROSS_COMPILE=$CROSS_COMPILE PLATFORM=generic FW_OPTIONS=0x2 BUILD_INFO=y V=1 -j$JOBS -s
 cp build/platform/generic/firmware/fw_dynamic.bin $SBI_OUT
 cp build/platform/generic/firmware/fw_dynamic.elf $SBI_ELF
